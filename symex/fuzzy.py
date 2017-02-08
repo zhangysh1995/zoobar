864eb9f99453dccbc7ba10d131dbfca0682caac0
foreverbell <dql.foreverbell@gmail.com>
2017-02-08 11:10:14 +0000

    Lab3: concolic_int implementation.

--- symex/fuzzy.py
+++ symex/fuzzy.py
@@ -187,8 +187,13 @@ class sym_minus(sym_binop):
   def _z3expr(self, printable):
     return z3expr(self.a, printable) - z3expr(self.b, printable)
 
-## Exercise 2: your code here.
-## Implement AST nodes for division and multiplication.
+class sym_mul(sym_binop):
+  def _z3expr(self, printable):
+    return z3expr(self.a, printable) * z3expr(self.b, printable)
+
+class sym_div(sym_binop):
+  def _z3expr(self, printable):
+    return z3expr(self.a, printable) / z3expr(self.b, printable)
 
 ## String operations
 
@@ -503,8 +508,27 @@ class concolic_int(int):
     res = o - self.__v
     return concolic_int(sym_minus(ast(o), ast(self)), res)
 
-  ## Exercise 2: your code here.
-  ## Implement symbolic division and multiplication.
+  def __mul__(self, o):
+    if isinstance(o, concolic_int):
+      res = self.__v * o.__v
+    else:
+      res = self.__v * o
+    return concolic_int(sym_mul(ast(self), ast(o)), res)
+
+  def __rmul__(self, o):
+    res = o * self.__v
+    return concolic_int(sym_mul(ast(o), ast(self)), res)
+
+  def __div__(self, o):
+    if isinstance(o, concolic_int):
+      res = self.__v / o.__v
+    else:
+      res = self.__v / o
+    return concolic_int(sym_div(ast(self), ast(o)), res)
+
+  def __rdivl__(self, o):
+    res = o / self.__v
+    return concolic_int(sym_div(ast(o), ast(self)), res)
 
   def _sym_ast(self):
     return self.__sym
